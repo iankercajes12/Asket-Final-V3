88953bfa67ad41d1f966e817400d583ede0fb12f
iankercajes12 <40867950+iankercajes12@users.noreply.github.com>
2024-12-30 09:45:29 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   let InstanceId_ReviewsTab = "";
 
   //Product ID
-  let ProductId = "TEE-MA";
+  let ProductId = "";
 
   //Clipboard text here.
   let ClipBoardHeading = "Follow these steps to add this";
